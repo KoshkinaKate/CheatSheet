9df475af784c55cc14c1c7f358b2e658b52e6b6f
Ekaterina Koshkina <ekaterinakoshkina@mbp-ekaterina.mynetworksettings.com>
2024-02-22 15:21:42 -0500

    Project #3

---
+++ app.js
@@ -0,0 +1,8 @@
+
+
+//keyword ---name--- () ==> {}
+const changeButton = ()=>{
+    //this is where we put instructions
+    let ourButton= document.querySelectorAll('lowContainer')
+    ourButton.style.backgroundColor = "salmon"
+}
